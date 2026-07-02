6e04aa7453bfbd8dcb958d9458ee84b29691a208
Hinson7886 <JonHinsonFX@gmail.com>
2026-07-01 22:12:34 -0400

    Add Project 3 files

--- Course.java
+++ Course.java
@@ -1,5 +1,6 @@
-/* First step: 
-Create the class. Once the class is created, we can use the class to create 
+/* 
+Create the Course class. The Course class is an aggregate class because it contains a TextBook object. 
+The course "has a" textbook; creating a "has a" relationship. Once the class is created, we can use the class to create 
 objects in the demo program.*/
 
 public class Course
@@ -7,119 +8,177 @@ public class Course
 {
 
    //fields 
-   /* We use the private access modifier because the class is "hiding" its data from outside code.
-   The use of the private access modifier protects data from corruption. 
-   
-   When an object is created in the program, the object will
-   have it's own "instance" fields: courseNumber, courseName, contactHours, creditHours.*/
-   
+  
    private String courseNumber, courseName; 
    private double contactHours, creditHours;
-   private final double COST_PER_HR = 72; 
+   private final double COST_PER_HR = 72;
+   private TextBook textbook; //Instance of the Textbook class
    
    
    
-   /*The constructor has the same name as the class.
-   The constructor will be used to "construct" or "create" an object in the demo program. It will be used to "store" 
-   initial values in the object's instance fields */
+   /* Constructor to initialize fields
+   @param cNum - course number
+   @param cName - course name
+   @param contHrs - course contact hours
+   @param credHrs - course credit hours
+   @param book - a TextBook object
+   
+   */
      
-   public Course( String cNum, String cName, double contHrs, double credHrs)
+   public Course( String cNum, String cName, double contHrs, double credHrs, TextBook book)
    {
    
-   /*An object will be created by calling the constructor in the demo program. 
-     The constructor in the demo program will accept 4 arguments. These arguments will be passed to the four 
-     parameter variables (parameter variables in this example: cNum, cName, contHrs, credHrs). The parameter variables are assigned
-     to the "instance" fields that belong to the object. */
-     
+   
       courseNumber = cNum;
       courseName = cName;
       contactHours = contHrs;
       creditHours = credHrs;
+      textbook = new TextBook(book);//we use the copy contructor of the TextBook class to avoid security holes
    }
    
    
    //Setter methods 
       
-   /*When an object is created in the demo program, the object will
-   have it's own "instance" methods: setCourseNumber, setCourseName, setContactHours, setCreditHours)*/
-
-   /*The setter methods are "void." We are not returning data to a statement that may call the method in the demo program.
-   The set methods have a parameter variable (i.e., String cNum, for example is the parameter variable for the
-   setCourseNumber method. If the course number changes in the program, the setCourseNumber method can be called to 
-   change the course number.*/
-         
-   /* We can use the set methods in the demo program to "change" a value in an instance field.*/
-   
-   /* Please note: If a no arg constructor is added to this class, you can call a no-arg constructor in the demo program 
-   and use the "set" methods to store a value in an instance field. In this class, we do not have a 
-   no-arg constructor, our constructor accepts arguments. */
-
-      
+   /**
+      @param cNum - the course number
+  */  
    public void setCourseNumber(String cNum)
    {
       courseNumber = cNum;
    }
+
+   /**
+      @param cName - the course name
+  */  
+
 
    public void setCourseName(String cName)
    {
       courseName = cName;
    }
 
-   public void setContactHours(double cHours)
+   /**
+      @param cHours - the contact hours  
+   */  
+
+
+   public void setContactHours(double contHrs)
    {
-      contactHours = cHours;
+      contactHours = contHrs;
    }
 
-   public void setCreditHours(double cHours)
-   {
-      creditHours = cHours;
-   }
+   /**
+      @param creditHours - the credit hours  */  
 
-   //Getters
-   /* Our instance fields will be private; therefore, we must have getter methods to "get" the values 
-   stored in an object's fields. We must include a "return" statement so the value is returned
-   without generating an error.*/
+
+   public void setCreditHours(double credHrs)
+   {
+      creditHours = credHrs;
+   }
    
-   /*When an object is created in the program, the object will
-   have it's own "instance" methods: getCourseNumber, getCourseName, getContactHours, getCreditHours*/
+   /**
+      @param book - a TextBook object  
+   */  
+   
+   public void setTextBook(TextBook book)
+   {
+      textbook = new TextBook(book);
+   }
 
+ //Getters
+   
+   /**
+      getCourseNumber method
+      @return - the value in the courseNumber field
+   */
+   
    public String getCourseNumber()
    {
       return courseNumber;
    }
 
+  /**
+      getCourseName method
+      @return - the value in the courseName field
+  */
+
    public String getCourseName()
    {
       return courseName;
    }
+ 
    
-      
+   /**
+      getContactHours method
+      @return - the value in the contactHours field
+   */
+   
    public double getContactHours()
    {
       return contactHours;
    }
    
+   /**
+      getCreditHours method
+      @return - the value in the creditHours field
+   */
+
    public double getCreditHours()
    {
       return creditHours;
    }
+   
+  /**
+      getTextBook method
+      @return - return a reference to a copy of this course's TextBook object
+  */
 
-   /* Perform the calculation to determine the tuition
-   Notice that COST_PER_HR was declared at the top of the class. It can be used by the getTuitionCost method because
-   fields are "global" in a class. 
+   
+   public TextBook getTextBook()
+   {
+      return new TextBook(textbook); 
+   }
 
-   Please note: COST_PER_HR could have been declared as a "local" variable within the getTuitionCost method. This is acceptable
-   because we are using COST_PER_HR only to calculate the tuition cost. 
-
-   We want to avoid having "stale data" by not assigning the tuition cost to a field in the method. We use only a return 
-   statement to return the result of the calculation.*/
-     
+   /**
+      getTuitionCost method
+      @return - return the value from the calculation
+      
+   */
+    
    public double getTuitionCost()
    {
       return creditHours * COST_PER_HR;
    }
    
+   /**
    
+      getTotalPrice method
+      @return - return the value from the calculation       
+   */
+   
+   public double getTotalPrice()
+   {
+      return getTuitionCost() + textbook.getPrice();
+   }
+   
+   /**
+      toString method
+      @return - A string containing the course information
+  */ 
+      
+  /** We make use of the String.format method to format the tuition cost and total cost. See Chapter 3 for information about this method.*/
+   
+   public String toString()
+   {
+      return String.format("Course Name: " + courseName +
+             "\nCourse Number: " + courseNumber +
+             "\nContact Hours: " + contactHours +
+             "\nCredit Hours: " + creditHours +
+             "\nTuition Cost: $%.2f" +
+             "\nText Book: " + textbook.toString() + //we use the TextBook's toString() method for convenience here
+             "\nTotal Price: $%.2f \n", getTuitionCost(), getTotalPrice());
+         
+   }
       
 //close the class     
 } 
